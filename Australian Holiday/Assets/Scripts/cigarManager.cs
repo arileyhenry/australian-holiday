4793ff3805c30874aee750de5d67c91049a14295
arileyhenry <arileyhenry@gmail.com>
2021-04-06 22:37:13 -0500

    GameUpdate

---
+++ Australian Holiday/Assets/Scripts/cigarManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cigarManager : MonoBehaviour
+{
+
+	[SerializeField]
+	private float time = 1.5f;
+
+	public GameObject ciggie;
+
+
+	void Start() {
+		StartCoroutine(SpawnAnEnemy());
+	}
+
+	IEnumerator SpawnAnEnemy() {
+
+		Vector2 spawn = GameObject.Find("Player").transform.position;
+		spawn += Random.insideUnitCircle.normalized * 3;
+		spawn.y = 0;
+
+		Instantiate(ciggie, spawn, Quaternion.identity);
+		yield return new WaitForSeconds(time);
+		StartCoroutine(SpawnAnEnemy());
+
+	}
+}
